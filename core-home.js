accc88c4b7b97ee3f9cc42431c9e173b81b057e5
Brandon Barnard <brandon@todaysdailybattle.com>
2026-08-15 08:03:47 -0500

    fix(site): red letters always on, never a setting
    Jesus’ words stay red. Remove the Appearance switch, ignore any
    saved off state, and hide leftover toggles.

--- core-home.js
+++ core-home.js
@@ -16,7 +16,7 @@
     }
   } catch (_) {}
 
-  var INTERACTIVE_SRC = 'script.js?v=20260815-red-now';
+  var INTERACTIVE_SRC = 'script.js?v=20260815-red-always';
   var loaded = false;
   var pendingQuery = '';
 
